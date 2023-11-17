0444def796dffb8b2f44db7bbe00b047e14b7a36
Diwas Rimal <rimaldiwas10@gmail.com>
2023-11-17 16:35:48 +0545

    Make proper move generator

--- chess.c
+++ chess.c
@@ -47,7 +47,7 @@ const MoveFlag QUEEN_PROMO_CAPTURE = QUEEN_PROMOTION | CAPTURE;
 // 4 bits for flag, 6, 6 for src and dst squares
 typedef uint16_t Move;
 const Move EMPTY_MOVE = 0;
-const Move FLAG_MASK = 15 << 12;
+const Move MFLAG_MASK = 15 << 12;
 const Move SRC_SQ_MASK = 63 << 6;
 const Move DST_SQ_MASK = 63;
 
@@ -56,6 +56,11 @@ typedef struct {
     Move moves[256];
     size_t count;
 } MoveList;
+
+// Max 64 squares could be attacked
+typedef struct {
+    int attacks[64];
+} AttackMap;
 
 typedef enum {
     RIGHT,
@@ -81,7 +86,7 @@ typedef struct {
 typedef unsigned long long Ull;
 
 int squareNameToIdx(char *name);
-void findSquareName(int square, char *name);
+void fillSquareName(int square, char *name);
 char pieceToNotation(const Piece p);
 bool isValidSquare(int rank, int file);
 bool haveSameColor(Piece p1, Piece p2);
@@ -89,15 +94,28 @@ Ull decToBin(int n);
 void populateSquaresTillEdges(void);
 Board initBoardFromFen(char *starting_fen);
 void printBoard(const Board b);
+bool isKingChecked(Board b);
 MoveList generateMoves(const Board b);
 MoveList generateSingleMoves(const Board b, int src_sq);
 void fillSlidingMoves(const Board b, int src_sq, MoveList *list);
 void fillPawnMoves(const Board b, int src_sq, MoveList *list);
 void fillKnightMoves(const Board b, int src_sq, MoveList *list);
 void fillKingMoves(const Board b, int src_sq, MoveList *list);
+void fillAttacks(const Board b, int *attacks);
+void fillSingleAttacks(const Board b, int src_sq, int *attacks);
+void fillSlidingAttacks(const Board b, int src_sq, int *attacks);
+void fillPawnAttacks(const Board b, int src_sq, int *attacks);
+void fillKnightAttacks(int src_sq, int *attacks);
+void fillKingAttacks(int src_sq, int *attacks);
+MoveFlag getMoveFlag(Move m);
+int getMoveSrc(Move m);
+int getMoveDst(Move m);
 Move moveEncode(MoveFlag flag, int src_sq, int dst_sq);
-void moveDecode(Move m, MoveFlag *flag, int *src_sq, int *dst_sq);
 Board moveMake(Move m, Board b);
+void printMoves(const MoveList move_list);
+int testGenerationTillDepth(Board b, int depth);
+
+int DEPTH = 5;
 
 int main(int argc, char **argv)
 {
@@ -108,26 +126,12 @@ int main(int argc, char **argv)
 
     Board b = initBoardFromFen(fen);
     printBoard(b);
-    MoveList move_list = generateMoves(b);
 
-    char src[3];
-    char dst[3];
-    MoveFlag flag;
-    int src_sq, dst_sq;
-    for (size_t i = 0; i < move_list.count; i++) {
-        Move m = move_list.moves[i];
-        moveDecode(m, &flag, &src_sq, &dst_sq);
-        findSquareName(src_sq, src);
-        findSquareName(dst_sq, dst);
-        printf("%s%s[%04llu] ", src, dst, decToBin(flag));
-    }
-    printf("\n");
+    // MoveList moves = generateMoves(b);
+    // printMoves(moves);
 
-    src_sq = squareNameToIdx("b1");
-    dst_sq = squareNameToIdx("c3");
-
-    printf("samecolor: %d\n",
-           haveSameColor(b.pieces[src_sq], b.pieces[dst_sq]));
+    int total = testGenerationTillDepth(b, DEPTH);
+    printf("Depth %d, moves: %d\n", DEPTH, total);
 }
 
 int squareNameToIdx(char *name)
@@ -137,7 +141,7 @@ int squareNameToIdx(char *name)
     return rank * 8 + file;
 }
 
-void findSquareName(int square, char *name)
+void fillSquareName(int square, char *name)
 {
     if (!isValidSquare(square / 8, square % 8)) {
         sprintf(name, "-");
@@ -319,63 +323,104 @@ Board initBoardFromFen(char *starting_fen)
 void printBoard(const Board b)
 {
     for (int rank = 7; rank >= 0; rank--) {
-        printf("%d |", rank + 1);
+        printf("   +---+---+---+---+---+---+---+---+\n");
+        printf(" %d |", rank + 1);
         for (int file = 0; file < 8; file++) {
-            printf(" %c ", pieceToNotation(b.pieces[rank * 8 + file]));
+            printf(" %c |", pieceToNotation(b.pieces[rank * 8 + file]));
         }
         printf("\n");
     }
-    printf("---------------------------\n");
-    printf("  |");
+    printf("   +---+---+---+---+---+---+---+---+\n");
+    printf("  ");
     for (char file = 'a'; file <= 'h'; file++) {
-        printf(" %c ", file);
+        printf("   %c", file);
     }
     printf("\n");
 
     char epsquare_name[3];
-    findSquareName(b.ep_square, epsquare_name);
+    fillSquareName(b.ep_square, epsquare_name);
     printf("turn: %c, castle rights: %04llu, ep square: %s, halfmove_clock: "
            "%d, fullmoves: %d\n",
            ((b.color_to_move & WHITE) ? 'w' : 'b'), decToBin(b.castle_rights),
            epsquare_name, b.halfmove_clock, b.fullmoves);
 }
 
-MoveList generateMoves(const Board b)
+// Given a board, finds if opposing king is in check
+bool isKingChecked(Board b)
 {
-    MoveList all_moves = {.moves = {EMPTY_MOVE}, .count = 0};
-
-    for (int rank = 0; rank < 8; rank++) {
-        for (int file = 0; file < 8; file++) {
-            int src_sq = rank * 8 + file;
-            if (b.pieces[src_sq] == EMPTY_PIECE ||
-                !haveSameColor(b.color_to_move, b.pieces[src_sq]))
-                continue;
-
-            MoveList list = generateSingleMoves(b, src_sq);
-            for (size_t i = 0; i < list.count; i++) {
-                all_moves.moves[all_moves.count++] = list.moves[i];
-            }
+    int king_sq = -1;
+    for (int src_sq = 0; src_sq < 64; src_sq++) {
+        if (b.pieces[src_sq] & KING &&
+            !haveSameColor(b.color_to_move, b.pieces[src_sq])) {
+            king_sq = src_sq;
+            break;
         }
     }
-    return all_moves;
+
+    if (king_sq == -1)
+        assert(0 && "King not found!");
+
+    // Set turn to opposite color to find attacks to our color
+    int attacks[64] = {0};
+    fillAttacks(b, attacks);
+
+    // printf("\nAttacks on isKingChecked\n");
+    // for (int r = 7; r >= 0; r--) {
+    //     for (int f = 0; f < 8; f++) {
+    //         printf("%d ", attacks[r * 8 + f]);
+    //     }
+    //     printf("\n");
+    // }
+
+    if (attacks[king_sq] > 0)
+        return true;
+
+    return false;
+}
+
+MoveList generateMoves(const Board b)
+{
+    MoveList pseudolegals = {.count = 0};
+
+    for (int src_sq = 0; src_sq < 64; src_sq++) {
+        if (b.pieces[src_sq] == EMPTY_PIECE ||
+            !haveSameColor(b.color_to_move, b.pieces[src_sq]))
+            continue;
+
+        MoveList list = generateSingleMoves(b, src_sq);
+        for (size_t i = 0; i < list.count; i++) {
+            pseudolegals.moves[pseudolegals.count++] = list.moves[i];
+        }
+    }
+
+    // Filter moves that will leave our king in check
+    MoveList legalmoves = {.count = 0};
+    for (size_t i = 0; i < pseudolegals.count; i++) {
+        Move m = pseudolegals.moves[i];
+        Board updated = moveMake(m, b);
+        if (isKingChecked(updated))
+            continue;
+
+        legalmoves.moves[legalmoves.count++] = m;
+    }
+
+    return legalmoves;
 }
 
 MoveList generateSingleMoves(const Board b, int src_sq)
 {
-    MoveList list = {.moves = {EMPTY_MOVE}, .count = 0};
+    MoveList list = {.count = 0};
 
-    // if (b.pieces[src_sq] & (ROOK|QUEEN|BISHOP)) {
-    //     fillSlidingMoves(b, src_sq, &list);
-    // }
-    // else if (b.pieces[src_sq] & PAWN) {
-    // if (b.pieces[src_sq] & PAWN) {
-    //     fillPawnMoves(b, src_sq, &list);
-    // }
-    // else if (b.pieces[src_sq] & KNIGHT) {
-    //     fillKnightMoves(b, src_sq, &list);
-    // }
-    // else if (b.pieces[src_sq] & KING) {
-    if (b.pieces[src_sq] & KING) {
+    if (b.pieces[src_sq] & (ROOK | QUEEN | BISHOP)) {
+        fillSlidingMoves(b, src_sq, &list);
+    }
+    else if (b.pieces[src_sq] & PAWN) {
+        fillPawnMoves(b, src_sq, &list);
+    }
+    else if (b.pieces[src_sq] & KNIGHT) {
+        fillKnightMoves(b, src_sq, &list);
+    }
+    else if (b.pieces[src_sq] & KING) {
         fillKingMoves(b, src_sq, &list);
     }
 
@@ -577,17 +622,107 @@ void fillKingMoves(const Board b, int src_sq, MoveList *list)
     }
 }
 
+void fillAttacks(const Board b, int *attacks)
+{
+    for (int src_sq = 0; src_sq < 64; src_sq++) {
+        if (b.pieces[src_sq] == EMPTY_PIECE ||
+            !haveSameColor(b.color_to_move, b.pieces[src_sq]))
+            continue;
+        fillSingleAttacks(b, src_sq, attacks);
+    }
+}
+
+void fillSingleAttacks(const Board b, int src_sq, int *attacks)
+{
+    if (b.pieces[src_sq] & (ROOK | QUEEN | BISHOP)) {
+        fillSlidingAttacks(b, src_sq, attacks);
+    }
+    else if (b.pieces[src_sq] & PAWN) {
+        fillPawnAttacks(b, src_sq, attacks);
+    }
+    else if (b.pieces[src_sq] & KNIGHT) {
+        fillKnightAttacks(src_sq, attacks);
+    }
+    else if (b.pieces[src_sq] & KING) {
+        fillKingAttacks(src_sq, attacks);
+    }
+}
+
+void fillSlidingAttacks(const Board b, int src_sq, int *attacks)
+{
+    int start = 0, end = 8;
+    if (b.pieces[src_sq] & ROOK)
+        end = 4;
+    else if (b.pieces[src_sq] & BISHOP)
+        start = 4;
+
+    for (int direction = start; direction < end; direction++) {
+        int offset = DIRECTION_OFFSETS[direction];
+        for (int n = 0; n < SQUARES_TILL_EDGE[src_sq][direction]; n++) {
+
+            int dst_sq = src_sq + offset * (n + 1);
+            attacks[dst_sq]++;
+
+            // Further path blocked
+            if (b.pieces[dst_sq] != EMPTY_PIECE)
+                break;
+        }
+    }
+}
+
+void fillPawnAttacks(const Board b, int src_sq, int *attacks)
+{
+    // Pawn only attacks diagnoals
+    int color = (b.pieces[src_sq] & WHITE) ? 0 : 1;
+    Direction diagonals[2] = {
+        PAWN_DIAGNOALS[color][0],
+        PAWN_DIAGNOALS[color][1],
+    };
+
+    for (int i = 0; i < 2; i++) {
+        if (SQUARES_TILL_EDGE[src_sq][diagonals[i]] != 0) {
+            int dst_sq = src_sq + DIRECTION_OFFSETS[diagonals[i]];
+            attacks[dst_sq]++;
+        }
+    }
+}
+
+void fillKnightAttacks(int src_sq, int *attacks)
+{
+    int rank = src_sq / 8;
+    int file = src_sq % 8;
+
+    for (int i = 0; i < 8; i++) {
+        int r = rank + KNIGHT_RANK_OFFSETS[i];
+        int f = file + KNIGHT_FILE_OFFSETS[i];
+        if (isValidSquare(r, f)) {
+            int dst_sq = r * 8 + f;
+            attacks[dst_sq]++;
+        }
+    }
+}
+
+void fillKingAttacks(int src_sq, int *attacks)
+{
+    // King attacks everywhere
+    for (int direction = 0; direction < 8; direction++) {
+        if (SQUARES_TILL_EDGE[src_sq][direction] != 0) {
+            int dst_sq = src_sq + DIRECTION_OFFSETS[direction];
+            attacks[dst_sq]++;
+        }
+    }
+}
+
 Move moveEncode(MoveFlag flag, int src_sq, int dst_sq)
 {
     return ((Move)flag << 12) | ((Move)src_sq << 6) | dst_sq;
 }
 
-void moveDecode(Move m, MoveFlag *flag, int *src_sq, int *dst_sq)
-{
-    *flag = (m & FLAG_MASK) >> 12;
-    *src_sq = (m & SRC_SQ_MASK) >> 6;
-    *dst_sq = m & DST_SQ_MASK;
-}
+MoveFlag getMoveFlag(Move m) { return (m & MFLAG_MASK) >> 12; }
+
+int getMoveSrc(Move m) { return (m & SRC_SQ_MASK) >> 6; }
+
+int getMoveDst(Move m) { return m & DST_SQ_MASK; }
 
 // Rook's src and dst squares during castle
 // idx 0 for white, 1 for black
@@ -598,20 +733,21 @@ const int KSC_ROOK_DST_SQ[2] = {5, 61};
 
 Board moveMake(Move m, Board b)
 {
-    MoveFlag flag;
-    int src_sq, dst_sq;
-    moveDecode(m, &flag, &src_sq, &dst_sq);
+    MoveFlag flag = getMoveFlag(m);
+    int src_sq = getMoveSrc(m);
+    int dst_sq = getMoveDst(m);
     int color = (b.pieces[src_sq] & WHITE) ? 0 : 1;
 
     // Record / reset en passantable square
     int pawn_forward_offset = DIRECTION_OFFSETS[PAWN_FORWARDS[color]];
-    if (flag & DOUBLE_PAWN_PUSH)
+    if (flag == DOUBLE_PAWN_PUSH) {
         b.ep_square = dst_sq - pawn_forward_offset;
+    }
     else
         b.ep_square = -1;
 
     // Capture pawn below dst square during en passant
-    if (flag & EP_CAPTURE)
+    if (flag == EP_CAPTURE)
         b.pieces[dst_sq - pawn_forward_offset] = EMPTY_PIECE;
 
     // Revoke castle rights if king is moving
@@ -619,11 +755,11 @@ Board moveMake(Move m, Board b)
         b.castle_rights ^= (KSC_FLAGS[color] | QSC_FLAGS[color]);
 
     // Move rook when castled
-    if (flag & QUEEN_CASTLE) {
+    if (flag == QUEEN_CASTLE) {
         b.pieces[QSC_ROOK_DST_SQ[color]] = b.pieces[QSC_ROOK_SRC_SQ[color]];
         b.pieces[QSC_ROOK_SRC_SQ[color]] = EMPTY_PIECE;
     }
-    else if (flag & KING_CASTLE) {
+    else if (flag == KING_CASTLE) {
         b.pieces[KSC_ROOK_DST_SQ[color]] = b.pieces[KSC_ROOK_SRC_SQ[color]];
         b.pieces[KSC_ROOK_SRC_SQ[color]] = EMPTY_PIECE;
     }
@@ -639,7 +775,6 @@ Board moveMake(Move m, Board b)
         b.castle_rights ^= KSC_FLAGS[color];
     }
 
-
     // Update or rest halfmove clock
     b.halfmove_clock++;
     if (flag & CAPTURE || b.pieces[src_sq] & PAWN)
@@ -650,11 +785,10 @@ Board moveMake(Move m, Board b)
 
     // Dst piece changes during promotion
     if (flag & PROMOTION) {
-        Piece promoted_piece = 
-            (flag & KNIGHT_PROMOTION) ? KNIGHT : 
-            (flag & BISHOP_PROMOTION) ? BISHOP : 
-            (flag & ROOK_PROMOTION) ? ROOK : 
-            QUEEN;
+        Piece promoted_piece = (flag == KNIGHT_PROMOTION)   ? KNIGHT
+                               : (flag == BISHOP_PROMOTION) ? BISHOP
+                               : (flag == ROOK_PROMOTION)   ? ROOK
+                                                            : QUEEN;
         dst_piece = b.color_to_move | promoted_piece;
     }
 
@@ -667,4 +801,45 @@ Board moveMake(Move m, Board b)
         b.fullmoves++;
 
     return b;
+}
+
+void printMoves(const MoveList move_list)
+{
+    char src[3];
+    char dst[3];
+    printf("Total moves: %lu\n", move_list.count);
+    for (size_t i = 0; i < move_list.count; i++) {
+        Move m = move_list.moves[i];
+        MoveFlag flag = getMoveFlag(m);
+        int src_sq = getMoveSrc(m);
+        int dst_sq = getMoveDst(m);
+        fillSquareName(src_sq, src);
+        fillSquareName(dst_sq, dst);
+        printf("%s%s[%04llu] ", src, dst, decToBin(flag));
+    }
+    printf("\n");
+}
+
+int testGenerationTillDepth(Board b, int depth)
+{
+    if (depth == 0)
+        return 1;
+
+    int total = 0;
+    char src[3], dst[3];
+    MoveList list = generateMoves(b);
+
+    for (size_t i = 0; i < list.count; i++) {
+        Board new = moveMake(list.moves[i], b);
+        int src_sq = getMoveSrc(list.moves[i]);
+        int dst_sq = getMoveDst(list.moves[i]);
+        fillSquareName(src_sq, src);
+        fillSquareName(dst_sq, dst);
+        int n_moves = testGenerationTillDepth(new, depth - 1);
+        if (depth == DEPTH)
+            printf("%s%s: %d\n", src, dst, n_moves);
+        total += n_moves;
+    }
+
+    return total;
 }
